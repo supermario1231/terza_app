fb4f94c8fa60954516b9923ba41c89bed4ac5882
mario lopresti <mario841ct@hotmail.it>
2011-05-15 11:11:44 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TerzaApp::Application.config.secret_token = '8a2dcfb6fb3d39a161da4f62e50f77a16dca007bf95b1110b04ec9b06b1a8dbcae39cda37aedb208540b81b485e3bc824d022632cc7cd221034439512994ae89'
